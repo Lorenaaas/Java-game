7ad2fd0fbcb846479b4b99b8c4a4fa18f1e7ad9e
Lorenaaas <>
2023-04-30 11:01:31 +0000

    add event listener to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log("happybirthday");
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div =  document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+const lizard_div = document.getElementById("lizard");
+const spock_div = document.getElementById("spock");
+
+rock_div.addEventListener('click', function() {
+    game("rock");
+});
+
+paper_div.addEventListener('click', function() {
+   game("paper");
+});
+scissors_div.addEventListener('click', function() {
+    game("scissors");
+});
+lizard_div.addEventListener('click', function() {
+    game("lizard");
+});
+
+spock_div.addEventListener('click', function() {
+   game("spock");
+});
+
