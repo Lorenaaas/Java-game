8f5397d8e38a8b21c529b3c564d43d96b245daab
Lorenaaas <>
2023-04-30 12:54:25 +0000

    Add functions for win lose and tie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,73 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div =  document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 const lizard_div = document.getElementById("lizard");
 const spock_div = document.getElementById("spock");
 
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     const randomNumber = Math.floor(Math.random() * 5);
-    return choices [randomNumber];
+    return choices[randomNumber];
 }
 
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${userChoice} beats ${computerChoice} .You win!`;
+}
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+   result_p.innerHTML = `${userChoice} loses to ${computerChoice} .You lost!`;
+}
+function tie(userChoice, computerChoice) {
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+   result_p.innerHTML = `${userChoice} equals ${computerChoice} .It's a tie!`;
+}
 
 function game(userChoice) {
    const computerChoice = getComputerChoice();
-   switch(userChoice + computerChoice) {
-     case "scissorspaper":
-     case "paperrock":
-     case "rocklizard":
-     case "lizardspock":
-     case "spockscissors":
-     case "scissorslizard":
-     case "lizardpaper":
-     case "paperspock":
-        console.log("USER IS THE WINNER!");
-        break;
-    case "paperscissors":
-    case "paperrock":
-    case "lizardrock":
-    case "spocklizard":
-    case "scissorsspock":
-    case "lizardscissors":
-    case "paperlizard":
-    case "spockpaper":
-        console.log("COMPUTER IS THE WINNER!");
-        break;
-    case "rockrock":
-    case "paperpaper":
-    case "scissorsscissors":
-    case "lizardlizard":
-    case "spockspock":
-        console.log("IT'S A TIE!");
-
+   switch (userChoice + computerChoice) {
+        case "scissorspaper":
+        case "paperrock":
+        case "rocklizard":
+        case "lizardspock":
+        case "spockscissors":
+        case "scissorslizard":
+        case "lizardpaper":
+        case "paperspock":
+            win(userChoice, computerChoice);
+            break;
+        case "paperscissors":
+        case "paperrock":
+        case "lizardrock":
+        case "spocklizard":
+        case "scissorsspock":
+        case "lizardscissors":
+        case "paperlizard":
+        case "spockpaper":
+            lose(userChoice, computerChoice);
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+        case "lizardlizard":
+        case "spockspock":
+            tie(userChoice, computerChoice);
+            break;
    }
+}
 
 function main() {
   rock_div.addEventListener('click', function() {
@@ -68,6 +88,3 @@ function main() {
    game("spock");
 });
 }
-
-main();
-
